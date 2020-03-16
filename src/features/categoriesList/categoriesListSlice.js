78639378e9dee663d225de181cedebad37a0cb58
Elena Donina <neonya@list.ru>
2020-03-17 00:15:12 +0100

    Spit category bar on components

--- src/features/categoriesList/categoriesListSlice.js
+++ src/features/categoriesList/categoriesListSlice.js
@@ -64,9 +64,6 @@ export const {
     toggleApprove
 } = slice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
 export const selectCategories = state => {
     return state.categories.categoriesList.filter(item => {
         if (
