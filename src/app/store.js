4f13c4e51aefdcaa08492c1d256ff7790a16900d
Elena Donina <neonya@list.ru>
2020-03-17 13:22:00 +0100

    remove useless import

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
-import {configureStore} from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import categoriesListReducer from '../features/categoriesList/categoriesListSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import categoriesListReducer from "../features/categoriesList/categoriesListSlice";
 
 export default configureStore({
     reducer: {
-        counter: counterReducer,
-        categories: categoriesListReducer,
-    },
+        categories: categoriesListReducer
+    }
 });
